67fc169b5e90f959f8a05a5befa536c88072c5c8
krithikvaidya <krithikvaidya@gmail.com>
2020-12-15 17:17:09 +0530

    Fix errors

--- replicated_kv_store/kv_store_node.go
+++ replicated_kv_store/kv_store_node.go
@@ -1,19 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
 	"net"
 	"time"
 
+	"github.com/krithikvaidya/distributed-dns/replicated_kv_store/protos"
 	"google.golang.org/grpc"
 )
 
-func main() {
+var n_replica int
+
+func init() {
+
+	flag.IntVar(&n_replica, "n", 5, "total number of replicas (default=5)")
+	flag.Parse()
 
 	log.SetFlags(0) // Turn off timestamps in log output.
 	rand.Seed(time.Now().UnixNano())
+
+}
+
+func main() {
 
 	fmt.Println("\nRaft-based Replicated Key Value Store\n")
 
@@ -32,9 +43,37 @@ func main() {
 	listener, err := net.ListenTCP("tcp", tcpAddr)
 	CheckError(err)
 
+	fmt.Printf("\nSuccessfully bound to address %v\n", address)
+
+	fmt.Printf("\nEnter the addresses of %v other replicas: \n", n_replica)
+
+	rep_addrs := make([]string, n_replica)
+
+	for i := 0; i < n_replica; i++ {
+
+		if i == rid {
+			continue
+		}
+
+		fmt.Scanf("%s", &rep_addrs[i])
+
+	}
+
 	grpcServer := grpc.NewServer()
 
+	node := InitializeNode(n_replica, rid)
+
+	protos.RegisterConsensusServiceServer(grpcServer, node)
+
 	err = grpcServer.Serve(listener)
-	checkError(err)
+	CheckError(err)
+
+	fmt.Printf("\ngRPC server listening...\n")
+
+	fmt.Printf("\nPress enter when all other nodes are online.\n")
+	var input rune
+	fmt.Scanf("%c", &input)
+
+	// node.ConnectToPeerReplicas(rep_addrs, rid)
 
 }
--- replicated_kv_store/raft_node.go
+++ replicated_kv_store/raft_node.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
 	"sync"
 	"time"
+
+	empty "github.com/golang/protobuf/ptypes/empty"
+	"github.com/krithikvaidya/distributed-dns/replicated_kv_store/protos"
 )
 
 type RaftNodeState int
@@ -15,22 +21,23 @@ const (
 )
 
 type RaftNode struct {
-	replica_id       int
-	peer_replica_addresses []string
-	raft_node_mutex  sync.Mutex
-	node_state       RaftNodeState
+	n_replicas             int
+	replica_id             int
+	peer_replica_addresses []net.Conn
+	raft_node_mutex        sync.Mutex
+	node_state             RaftNodeState
 
 	// States mentioned in figure 2 of the paper:
 
 	// State to be maintained on all replicas (TODO: persist)
 	currentTerm int
 	votedFor    int
-	log         []LogEntry
+	log         []int
 
 	// State to be maintained on all replicas
 	commitIndex        int
 	lastApplied        int
-	state              CMState
+	state              RaftNodeState
 	electionResetEvent time.Time
 
 	// State to be maintained on the leader
@@ -53,25 +60,52 @@ func GetState(state RaftNodeState) string {
 
 	}
 
+	return "Invalid"
+
 }
 
-func InitializeNode (rid int, praddrs []int) {
+var replicas_ready int
 
-	rn := &RaftNode {
+func InitializeNode(n_replica int, rid int) *RaftNode {
 
-		replica_id: rid,
-		peer_replica_addresses: praddrs,
-		node_state: Follower,
-		
-		currentTerm: 0,  // unpersisted
-		votedFor: -1,
-		log: make([]int, 10000)
+	replicas_ready = 0
+
+	rn := &RaftNode{
+
+		n_replicas:             n_replica,
+		replica_id:             rid,
+		peer_replica_addresses: make([]net.Conn, n_replica),
+		node_state:             Follower,
+
+		currentTerm: 0, // unpersisted
+		votedFor:    -1,
+		log:         make([]int, 10000),
 
 		commitIndex: 0,
 		lastApplied: 0,
-
 	}
 
 	return rn
+
+}
+
+func (node *RaftNode) ReplicaReady(ctx context.Context, in *empty.Empty) (*empty.Empty, error) {
+
+	log.Printf("\nReceived ReplicaReady\n")
+	return &empty.Empty{}, nil
+
+}
+
+func (node *RaftNode) RequestVote(ctx context.Context, in *protos.RequestVoteMessage) (*protos.RequestVoteResponse, error) {
+
+	log.Printf("\nReceived ReplicaReady\n")
+	return &protos.RequestVoteResponse{}, nil
+
+}
+
+func (node *RaftNode) AppendEntries(ctx context.Context, in *protos.AppendEntriesMessage) (*protos.AppendEntriesResponse, error) {
+
+	log.Printf("\nReceived ReplicaReady\n")
+	return &protos.AppendEntriesResponse{}, nil
 
 }
