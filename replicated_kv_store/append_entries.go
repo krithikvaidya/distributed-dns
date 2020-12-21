1fcf63270bedc22bc5158e3bc490cc1a7edc801a
Jason <jasonkrithik@gmail.com>
2020-12-21 12:14:20 +0530

    Made the changes and added comments

--- replicated_kv_store/append_entries.go
+++ replicated_kv_store/append_entries.go
@@ -15,30 +15,39 @@ func (node *RaftNode) AppendEntries(ctx context.Context, in *protos.AppendEntrie
 		// term received is lesser than current term
 		if int32(node.currentTerm) > in.Term {
 			return &protos.AppendEntriesResponse{Term: int32(node.currentTerm), Success: false}, nil
-		} else if int32(node.currentTerm) <= in.Term { // currect term is lesser than received term, we transition into being a candidate and reset timer and update term
-			node.currentTerm = int(in.Term)
-			node.node_state = Candidate
+		} else if int32(node.currentTerm) < in.Term { // currect term is lesser than received term, we transition into being a candidate and reset timer and update term
+			node.currentTerm = in.Term
+			node.state = Follower
 			node.electionResetEvent = time.Time{}
 		}
-		// we start from prevlogindex, check if stored values are equal to that in the leader if no, then it updates value and goes back doing the same till they are equal
-		i := in.PrevLogIndex
-		for ; int32(node.log[i]) != in.Entries[i] && i >= 0; i-- {
-			node.log[i] = int(in.Entries[i])
-		}
-		// if all entries arent in the node then it adds those values
-		for i = in.PrevLogIndex + 1; i < int32(len(in.Entries)); i++ {
-			node.log[i] = int(in.Entries[i])
-		}
-		//  If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
-		if len(in.Entries) > node.commitIndex {
-			if int32(len(in.Entries)) < in.PrevLogIndex+1 {
-				node.commitIndex = len(in.Entries)
-			} else {
-				node.commitIndex = int(in.PrevLogIndex)
+
+		if in.PrevLogIndex == -1 || node.log[in.PrevLogIndex].term == in.PrevLogTerm && in.PrevLogIndex {
+			logIndex := in.PrevLogIndex + 1
+			entryIndex := 0
+			for entryIndex < len(in.Entries) {
+				// we start from prevlogindex, check if stored values are equal to that in the leader if no, then it updates value and goes ahead doing the same
+				if node.log[logIndex].value != in.Entries[entryIndex].value || node.log[logIndex].term != in.Entries[entryIndex].term {
+					node.log[logIndex].value = in.Entries[entryIndex].value
+					node.log[logIndex].term = in.Entries[entryIndex].term
+					node.log[logIndex].operation = in.Entries[entryIndex].operation
+				}
+				logIndex++
+				entryIndex++
 			}
+
+			//  If leaderCommit > commitIndex, set commitIndex = min(leaderCommit, index of last new entry)
+			if in.leaderCommit > node.commitIndex {
+				if in.leaderCommit < in.PrevLogIndex+1 {
+					node.commitIndex = in.leaderCommit
+				} else {
+					node.commitIndex = in.PrevLogIndex
+				}
+			}
+			return &protos.AppendEntriesResponse{Term: int32(node.currentTerm), Success: true}, nil
+		} else { //Reply false if log doesn’t contain an entry at prevLogIndex whose term matches prevLogTerm (§5.3)
+			return &protos.AppendEntriesResponse{Term: int32(node.currentTerm), Success: false}, nil
 		}
+
 	}
 	// ...
-	return &protos.AppendEntriesResponse{Term: int32(node.currentTerm), Success: true}, nil
-
 }
