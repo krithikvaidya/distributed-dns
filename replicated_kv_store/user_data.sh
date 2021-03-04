ebba77c66768cde24f69f11ff606639c049989d0
krithikvaidya <krithikvaidya@gmail.com>
2021-03-04 09:52:23 +0530

    User data

--- replicated_kv_store/user_data.sh
+++ replicated_kv_store/user_data.sh
@@ -20,17 +20,17 @@ Content-Disposition: attachment; filename="userdata.txt"
 #!/bin/bash
 
 export REPLICA_ID=0
-export REP_0_GRPC_ADDR=172.31.74.22  # fill in, eg: ec2-23-21-123-241.compute-1.amazonaws.com:5000
-export REP_1_GRPC_ADDR=172.31.78.208
-export REP_2_GRPC_ADDR=172.31.70.118
+export REP_0_GRPC_ADDR=  # fill in, eg: ec2-23-21-123-241.compute-1.amazonaws.com:5000
+export REP_1_GRPC_ADDR=
+export REP_2_GRPC_ADDR=
 
-# store ARN of respective target group.
-export TG_ARN=arn:aws:elasticloadbalancing:us-east-1:991577021500:targetgroup/use1-root-tg/690a4ae0ae3d5f99
+# store ARN of respective target group. eg: arn:aws:elasticloadbalancing:us-east-1:991577021500:targetgroup/use1-root-tg/690a4ae0ae3d5f99
+export TG_ARN=
 
-# store instance IDs.
-export INST_ID_0=i-03e4aedd040c6675e
-export INST_ID_1=i-0b89e17038e91ed04
-export INST_ID_2=i-026c8bb28b265c37d
+# store AWS instance IDs.
+export INST_ID_0=
+export INST_ID_1=
+export INST_ID_2=
 
 cd /home/ubuntu/workspace/distributed-dns/replicated_kv_store
 ./replicated_kv_store -n 3
